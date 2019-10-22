7161bc71ba750891b533d45a1b2e11c29cc98e35
a-delmas <aurore.delmas@hotmail.fr>
2019-10-22 10:25:45 +0200

    Swith 1

--- static/script.js
+++ static/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    
-    $("#btnadd").click(function(){
-        $("#taskList").append("<li class='justify-content-between d-flex'><p>" + $("#task").val() + "</p><button class='delTask'><i class='fas fa-times-circle'></i></button></li>");       
+$(document).ready(function () {
+
+    $("#btnadd").click(function () {
+        $("#taskList").append("<li class='justify-content-between d-flex'><p>" + $("#task").val() + "</p><button class='delTask'><i class='fas fa-times-circle'></i></button></li>");
     })
-    
-    $(".delTask").click(function(){
+
+    $(".delTask").click(function () {
         $(this).parent().remove();
     })
-    
-    
-    $("taskElement").draggable();
-    $("#taskList").droppable();
 
+    $("#taskList").sortable();
+    $("#taskList").disableSelection();
 
-
+    $("#start").click(function(){
+        $("#taskTimer").append("#taskList:firstChild");
+    })
 
 
 });
