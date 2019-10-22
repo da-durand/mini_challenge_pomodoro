cd1fb97c58cdd74af4acd78f00428d9251350e3b
dadurand <ha.halo@live.fr>
2019-10-22 09:50:28 +0200

    drag and drop en cours

---
+++ static/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $("#btnadd").click(function(){
+        $("#taskList").append("<li class='justify-content-between d-flex'><p>" + $("#task").val() + "</p><button class='delTask'><i class='fas fa-times-circle'></i></button></li>");       
+    })
+    
+    $(".delTask").click(function(){
+        $(this).parent().remove();
+    })
+    
+    
+    $("taskElement").draggable();
+    $("#taskList").droppable();
+
+
+
+
+
+});
